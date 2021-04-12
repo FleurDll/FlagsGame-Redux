a9c1cc53e58917538881fd87ebe7bb708ee33c22
Fleur <fleur.dalle@hotmail.com>
2021-04-12 10:12:17 +0200

    remove the process.env

--- src/index.js
+++ src/index.js
@@ -8,17 +8,15 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
-require("dotenv").config();
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
     <Provider store={store}>
         <HarperDBProvider
-            url={process.env.REACT_APP_URL_DB}
-            user={process.env.REACT_APP_USER_DB}
-            password={process.env.REACT_APP_PASSWORD_DB}
+            url="https://cloud-flag-fleurtech.harperdbcloud.com"
+            user="FleurDll"
+            password="FlowerPower*18"
         >
             <App />
         </HarperDBProvider>
